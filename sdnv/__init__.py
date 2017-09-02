348d8e1de9ef328e86cfe026574029c227cb95bd
Marco 'don' Kaulea <DonMarco42@gmail.com>
2017-09-02 11:32:53 +0200

    Fix bug in handling offset >0
    When using an offset >0 the maximum length of the SDNV would not be
    calculated correctly and abort early.

--- sdnv/__init__.py
+++ sdnv/__init__.py
@@ -1,8 +1,15 @@
 
 
-def decode(buffer: bytearray, offset: int=0, max_length: int=None) -> int:
+def decode(buffer: bytearray, offset: int=0, max_length: int=9) -> (int, int):
+    """Decode a SDNV from a buffer
+
+    :param buffer: The buffer which contains a SDNV
+    :param offset: The offset into the buffer, at which the SDNV can be found
+    :param max_length: The maximum number of octets that make up the SDNV
+    :return: Decoded value and number of octets that made up the SDNV
+    """
     content = int()
-    for i, c in enumerate(buffer[offset:max_length]):
+    for i, c in enumerate(buffer[offset:offset+max_length]):
         content <<= 7
         content += c & 0x7F
         if not 0x80 & c:
@@ -13,6 +20,11 @@ def decode(buffer: bytearray, offset: int=0, max_length: int=None) -> int:
 
 
 def encode(content: int) -> bytearray:
+    """Encode an integer into a SDNV
+
+    :param content: Integer to encode
+    :return: Bytearray containing the encoded value
+    """
     if content < 0 or type(content) != int:
         raise ValueError("Invalid content")
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sdnv',
-    version='0.1.0',
+    version='0.2.0',
     description='Module to provide encoding/decoding of RFC6256 SDNV',
     long_description=open('README.rst').read(),
     author='Marco \'don\' Kaulea',
